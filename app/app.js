f4537164726e3a9582023ddad3790f51345a2391
ddosul-it <kids83@naver.com>
2021-09-28 17:35:02 +0900

    폴더구조 최적화

---
+++ app/app.js
@@ -0,0 +1,13 @@
+"use strict"
+
+//모듈
+const express = require("express");
+const app = express();
+
+
+//라우팅
+const home = require("./src/routes/home")
+
+app.use("/", home); //use -> 미들웨어 등록해 주는 메서드
+
+module.exports = app;
